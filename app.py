003b8a5bb1e68423b81669c0a52d92f3844cae7f
sanskritiii18 <sanskritiverma.1807@gmail.com>
2025-07-08 18:35:53 +0530

    added email service

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from crypt import methods
-
-from flask import Flask,render_template,flash,redirect,url_for
+from pyexpat.errors import messages
+from flask import Flask,render_template,flash
+from flask_mail import Mail, Message
 from forms import ContactMeForm
+import os
+from dotenv import load_dotenv
 
 app=Flask(__name__)
+load_dotenv()
 
+app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
+app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT"))
+app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS") == 'True'
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_DEFAULT_SENDER")
 
-
+mail=Mail(app)
 
 @app.route('/')
 def home_page():
@@ -24,15 +35,22 @@ def project_page():
 def contact_me_page():
     form =  ContactMeForm()
     if form.validate_on_submit():
-        #send it via email
+        name = form.name.data
+        email = form.email.data
+        subject =form.subject.data or 'NO SUBJECT'
+        message = form.message.data or 'No message'
+        # Email to you
+        msg = Message(subject=f"New Contact: {subject}",
+                      sender=email,
+                      recipients=['sanskritiverma.1807@gmail.com'],
+                      body=f"From: {name} <{email}>\n\nMessage:\n{message}")
+
+        mail.send(msg)
+
         flash('Thank you for your message!', 'success')
-        return redirect(url_for('success_page'))
+        return render_template('success.html')
 
     return  render_template('contact_me.html',form=form)
-
-@app.route('/success')
-def success_page():
-    return render_template('success.html')
 
 
 if __name__ == "__main__":
