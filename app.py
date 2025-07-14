217e6af1a856d7c603fa249368f1fae8372a19ef
sanskritiii18 <sanskritiverma.1807@gmail.com>
2025-07-14 22:39:14 +0530

    make production ready

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ from flask import Flask, render_template, flash, redirect, url_for,session
 from flask_mail import Mail, Message
 from werkzeug.utils import redirect
 from wtforms.validators import email
-
+import json
 from forms import ContactMeForm
 import os
 from dotenv import load_dotenv
+from flask_talisman import Talisman
+
 
 app=Flask(__name__)
 mail=Mail(app)
+Talisman(app)
+
 load_dotenv()
 
 app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
@@ -31,74 +35,16 @@ def home_page():
 
 @app.route('/Experience')
 def experience_page():
-    experience_list = [
-        {
-            "organization": "C-DAC (Centre for Development of Advanced Computing), Pune",
-            "role": "Applied AI Intern",
-            "duration": "March 2025 – Present",
-            "technologies": ["NLP", "Deep Learning"],
-            "confidential": True
-
-        },
-        {
-            "organization": "Desktop Buddha (Remote)",
-            "role": "Python Developer Intern",
-            "duration": "Jan 2025 – Feb 2025",
-            "technologies": ["Python", "Streamlit", "PostgreSQL", "Google OAuth2.0", "Pytest", "Selenium", "Git",
-                             "Jira"],
-            "responsibilities": [
-                "Developed core features using Streamlit and PostgreSQL",
-                "Implemented Google OAuth2.0 authentication",
-                "Built a feedback survey system with admin dashboard",
-                "Conducted testing using Pytest and Selenium",
-                "Participated in Agile sprints using Jira Scrum"
-            ]
-        }
-    ]
+    with open('data/experience.json') as f:
+        experience_list = json.load(f)
     return render_template('Experience.html', experiences=experience_list)
-
 
 
 
 @app.route('/project')
 def project_page():
-    projects = [
-        {
-            "title": "Data Visualization Dashboard",
-            "desc": "Built using MongoDB, PyMongo, Flask, Plotly & Folium. Displays insights via interactive graphs and maps.",
-            "tech": ["MongoDB", "PyMongo", "Flask", "Plotly", "Folium", "Pandas"],
-            "icon": "fa-chart-line",
-            "link": "http://github.com/sanskritiii18/data_visualization_dashboard"
-        },
-        {
-            "title": "Query Optimization Dashboard",
-            "desc": "Flask dashboard for analyzing MongoDB queries, execution time, and performance suggestions.",
-            "tech": ["Flask", "MongoDB", "HTML", "CSS", "JavaScript"],
-            "icon": "fa-database",
-            "link": "https://github.com/sanskritiii18/query_processing-optimization_dashboard"
-        },
-        {
-            "title": "Face Recognition Attendance",
-            "desc": "OpenCV + Python system to detect faces and record attendance in CSV.",
-            "tech": ["Python", "OpenCV", "CSV"],
-            "icon": "fa-face-smile",
-            "link": "https://github.com/sanskritiii18/face_recognition_attendace_system"
-        },
-        {
-            "title": "Hinglish Interpreter App",
-            "desc": "ML/NLP app that interprets mixed Hindi-English queries using tokenization and intent classification.",
-            "tech": ["Python", "NLP", "Machine Learning"],
-            "icon": "fa-language",
-            "link": "https://github.com/sanskritiverma/hinglish-interpreter"
-        },
-        {
-            "title": "E-Commerce App (Django)",
-            "desc": "Django e-commerce platform with PostgreSQL, Chart.js dashboards, Bootstrap UI, and authentication.",
-            "tech": ["Django", "PostgreSQL", "Bootstrap", "Chart.js"],
-            "icon": "fa-cart-shopping",
-            "link": "https://github.com/sanskritiverma/django-ecommerce"
-        }
-    ]
+    with open('data/projects.json', 'r') as f:
+        projects = json.load(f)
     return render_template('project.html', projects=projects)
 
 
@@ -126,6 +72,15 @@ def contact_me_page():
 @app.route('/success',methods=['GET'])
 def success_page():
     return render_template('success.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("500.html"), 500
 
 
 if __name__ == "__main__":
